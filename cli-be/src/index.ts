eaba5ef5f511ab4fd9c084e998af2d6f11b7a2e5
carl <carlos.acitores@colba.es>
2026-01-07 15:43:17 +0100

    secured routes

--- cli-be/src/index.ts
+++ cli-be/src/index.ts
@@ -1,9 +1,32 @@
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
+app.use('*', clerkMiddleware());
+app.use('/api/*', async (c, next) => {
+  const auth = getAuth(c);
+  if (!auth?.isAuthenticated) return c.status(401);
+  
+  await next();
+});
+
 app.get('/', (c) => c.text('Hello Hono!'))
+app.get('/api', (c) => {
+  const auth = getAuth(c)
+
+  if (!auth?.userId) {
+    return c.json({
+      message: 'You are not logged in.',
+    })
+  }
+
+  return c.json({
+    message: 'You are logged in!',
+    userId: auth.userId,
+  })
+});
 
 serve({
   fetch: app.fetch,
