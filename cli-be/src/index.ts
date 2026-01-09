8b180baf5c620a27e452f423940d09b5892a9199
carl <carlos.acitores@colba.es>
2026-01-09 20:57:06 +0100

    back to port listen

--- cli-be/src/index.ts
+++ cli-be/src/index.ts
@@ -14,10 +14,6 @@ app.use(express.json());
 
 app = mapSaveCliEndpoint(app);
 
-export default app;
-
-/*
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
 });
-*/
